dc652d3aced0c33ddad309b2592b87d4a1da1ddf
jamsheer <kuttypjamsheer@gmail.com>
2017-09-22 18:11:45 +0530

    Process Order - Start Finish Btn - Prototype Only

--- process_manufacturing/process_manufacturing/doctype/oztro_process_order/oztro_process_order.js
+++ process_manufacturing/process_manufacturing/doctype/oztro_process_order/oztro_process_order.js
@@ -2,6 +2,20 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Oztro Process Order', {
+	refresh: function(frm){
+		if(!frm.doc.__islocal && frm.doc.status == 'Open'){
+			var start_btn = frm.add_custom_button(__('Start'), function(){
+				start_process_order(frm)
+			});
+			start_btn.addClass('btn-primary');
+		}
+		if(!frm.doc.__islocal && frm.doc.status == 'Start'){
+			var finish_btn = frm.add_custom_button(__('Finish'), function(){
+				stop_process_order(frm)
+			});
+			finish_btn.addClass('btn-primary')
+		}
+	},
 	process_type: function(frm){
 		frm.set_query("process_name", function () {
 			return {
@@ -24,3 +38,40 @@ frappe.ui.form.on('Oztro Process Order', {
 		}
 	}
 });
+
+var start_process_order = function(frm){
+	me.frm.set_value("status", "Start");
+	//start_prompt()
+	cur_frm.save()
+}
+
+var stop_process_order = function(frm){
+	me.frm.set_value("status", "Finish");
+	cur_frm.save()
+}
+
+/*
+var start_prompt = function(frm) {
+	frappe.prompt({fieldtype:"Float", label: __("Qty for {0}", ["Processing"]), fieldname:"qty",
+		description: __("Max: {0}", [10]), 'default': 10 },
+		function(data) {
+			if(data.quantity > 10) {
+				frappe.msgprint(__("Quantity must not be more than {0}", [10]));
+				return;
+			}
+			frappe.call({
+				method:"erpnext.manufacturing.doctype.production_order.production_order.make_stock_entry",
+				args: {
+					"production_order_id": frm.doc.name,
+					"purpose": "Processing",
+					"qty": data.quantity
+				},
+				callback: function(r) {
+					
+					var doclist = frappe.model.sync(r.message);
+					frappe.set_route("Form", doclist[0].doctype, doclist[0].name);
+
+				}
+			});
+		}, __("Select Quantity"), __("Make"));
+}*/
