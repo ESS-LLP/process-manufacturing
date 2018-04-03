cf499bcccf1563762392baa51630ec742bf5f28e
jamsheer <kuttypjamsheer@gmail.com>
2018-04-03 11:39:45 +0530

    Doctype Oztro Process Order Item renamed to Process Order Item

--- process_manufacturing/process_manufacturing/doctype/process_order_item/process_order_item.py
+++ process_manufacturing/process_manufacturing/doctype/process_order_item/process_order_item.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2017, earthians and contributors
+# Copyright (c) 2018, earthians and contributors
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 
-class OztroProcessOrderItem(Document):
+class ProcessOrderItem(Document):
 	pass
