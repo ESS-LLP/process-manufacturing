8d1f2e3edf6d866184cd0f053ff428e7333ca9a6
Tyler Matteson <tlandonmatteson@gmail.com>
2018-11-26 17:40:46 -0500

    Fixed setup

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
-import re, ast
+import re
+from ast import literal_eval
+
+
+with open('requirements.txt') as r:
+	install_requires = r.read().strip().split('\n')
 
 # get version from __version__ variable in process_manufacturing/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
 with open('process_manufacturing/__init__.py', 'rb') as f:
-    version = str(ast.literal_eval(_version_re.search(
-        f.read().decode('utf-8')).group(1)))
-
-requirements = parse_requirements("requirements.txt", session="")
+	version = str(literal_eval(_version_re.search(f.read().decode('utf-8')).group(1)))
 
 setup(
 	name='process_manufacturing',
@@ -21,6 +22,5 @@ setup(
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=[str(ir.req) for ir in requirements],
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+	install_requires=install_requires
 )
