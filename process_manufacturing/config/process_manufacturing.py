d6c02775dbd254ed9305b54bfee21497357dda5b
Richard Andrew <richard.andrew811@gmail.com>
2021-07-15 16:33:51 +0700

    PM-2147 rearrange process manufacturing module and add dependencies

--- process_manufacturing/config/process_manufacturing.py
+++ process_manufacturing/config/process_manufacturing.py
@@ -1,55 +1,77 @@
 from __future__ import unicode_literals
 from frappe import _
 
+
 def get_data():
 	return [
 		{
-			"label": _("Production"),
-			"items": [
-                {
-					"type": "doctype",
-					"name": "Process Order",
-					"description": _("Process Manufacturing Order."),
-				},
-				{
-					"type": "doctype",
-					"name": "Stock Entry",
-					"description": _("Record item movement."),
-				},
-				{
-					"type": "doctype",
-					"name": "Material Request",
-					"description": _("Requests for items."),
-				},
-			]
-		},
-        {
 			"label": _("Process Manufacturing"),
 			"items": [
 				{
 					"type": "doctype",
-					"name": "Process Definition",
-					"description": _("Process Definition."),
+					"name": "Item",
+					"description": _("All Products or Services."),
+					"onboard": 1
+				},
+				{
+					"type": "doctype",
+					"name": "Batch",
+					"description": _("Batch (lot) of an Item."),
+					"onboard": 1,
+					"dependencies": ["Item"]
+				},
+				{
+					"type": "doctype",
+					"name": "Manufacturing Department",
+					"description": _("Manufacturing Department"),
+					"onboard": 1
+				},
+				{
+					"type": "doctype",
+					"name": "Workstation",
+					"description": _("Workstation"),
+					"onboard": 1,
+					"dependencies": ["Manufacturing Department"]
 				},
 				{
 					"type": "doctype",
 					"name": "Process Type",
 					"description": _("Process Type."),
+					"onboard": 1
 				},
-                {
+				{
 					"type": "doctype",
-					"name": "Manufacturing Department",
-					"description": _("Manufacturing Department"),
+					"name": "Process Definition",
+					"description": _("Process Definition."),
+					"onboard": 1,
+					"dependencies": ["Workstation", "Manufacturing Department", "Process Type", "Item"]
 				},
-                {
+			]
+		},
+		{
+			"label": _("Production"),
+			"items": [
+				{
 					"type": "doctype",
-					"name": "Item",
-					"description": _("All Products or Services."),
+					"name": "Process Order",
+					"description": _("Process Manufacturing Order."),
+					"onboard": 1,
+					"dependencies": ["Warehouse", "Workstation", "Manufacturing Department", "Process Type",
+									 "Process Definition"]
 				},
-                {
+				{
 					"type": "doctype",
-					"name": "Batch",
-					"description": _("Batch (lot) of an Item."),
+					"name": "Stock Entry",
+					"description": _("Record item movement."),
+					"onboard": 1,
+					"dependencies": ["Item"]
+				},
+				{
+					"type": "doctype",
+					"name": "Material Request",
+					"description": _("Requests for items."),
+					"onboard": 1,
+					"dependencies": ["Item"]
 				},
 			]
 		},
